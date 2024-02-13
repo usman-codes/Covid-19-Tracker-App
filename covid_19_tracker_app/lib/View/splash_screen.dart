245859059b9ba5e712ca2ac94aac8f2cc385bdc4
usman-codes <usmanamin31045@gmail.com>
2024-02-13 22:32:16 +0500

    initial-commit

---
+++ covid_19_tracker_app/lib/View/splash_screen.dart
@@ -0,0 +1,77 @@
+import 'dart:async';
+
+import 'package:covid_19_tracker_app/View/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'dart:math' as math;
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: Duration(seconds: 10), vsync: this)
+        ..repeat();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+  }
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+        Duration(seconds: 10),
+        () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => home_screen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AnimatedBuilder(
+                  animation: _controller,
+                  child: Center(
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      child: Image.asset("assets/virus.png"),
+                    ),
+                  ),
+                  builder: (BuildContext context, Widget? child) {
+                    return Transform.rotate(
+                      angle: _controller.value * 2.0 * math.pi,
+                      child: child,
+                    );
+                  }),
+              SizedBox(height: MediaQuery.of(context).size.height * .08),
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Covid-19 \n Tracker App",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.abhayaLibre(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
